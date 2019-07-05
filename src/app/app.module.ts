e0c95d917a9fcd692c1a0c1a67019674b5200222
Janice-M <janiceink001@gmail.com>
2019-07-05 11:19:57 +0300

    forms module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import {FormsModule}from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { QuoteFormComponent } from './quote-form/quote-form.component';
@@ -9,18 +9,19 @@ import { QuoteVoteComponent } from './quote-vote/quote-vote.component';
 import { TopDirective } from './top.directive';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    QuoteFormComponent,
-    QuotesComponent,
-    QuoteVoteComponent,
-    TopDirective
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+declarations: [
+  AppComponent,
+  QuoteFormComponent,
+  QuotesComponent,
+  QuoteVoteComponent,
+  TopDirective
+],
+imports: [
+  BrowserModule,
+  AppRoutingModule,
+  FormsModule
+],
+providers: [],
+bootstrap: [AppComponent]
 })
 export class AppModule { }
---
+++ src/app/quote.ts
@@ -0,0 +1,4 @@
+export class Quote {
+        lyrics: string;
+        name: string;
+}
